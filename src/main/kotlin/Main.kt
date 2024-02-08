95e0d5ef33d2dea5c5e9d2243f444a3351ac61bf
Gatchang-nyawargak <nyawargakgatchang@gmail.com>
2024-02-08 21:56:47 +0300

    added a file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val fullName: String = "Anna John"
+    val age: Int = 20
+    val phone: String = "+254741924309"
+    val weightInKg:Double = 76.5
+    val kenya:Boolean = true
+
+    println(fullName)
+    println(age)
+    println(phone)
+    println(weightInKg)
+    println(kenya)
+
+
+
+}
+
+
+
+
